90def8602f9185afa6aeae4ca4008695758bfcb4
Atharv Yadav <115143417+atharvbyadav@users.noreply.github.com>
2025-01-24 10:02:07 +0000

    Adding 3.18-Compute_Total_Marks again

--- 3.18-Compute_Total_Marks.cpp
+++ 3.18-Compute_Total_Marks.cpp
@@ -6,25 +6,29 @@ using namespace std;
 
 int main(){
 
-    int n, m, i, j, no, marks, total;
+    int num_students, num_subjects, roll_no, marks, total_marks;
 
-    cout << "\nEnter no. of Students and Subjects\n";
-    cin >> n >> m;
+    // Input the number of students and subjects
+    cout << "Enter the number of students and subjects: ";
+    cin >> num_students >> num_subjects;
 
-    for(i = 1; i <= n; ++i){
-        cout << "\nEnter Roll no: ";
-        cin >> no;
+    // Process each student
+    for (int i = 1; i <= num_students; ++i) {
+        cout << "\nEnter Roll Number for Student " << i << ": ";
+        cin >> roll_no;
 
-        total = 0;
-        cout << "\nEnter marks of " << m << " subjects for " << no << ":";
+        total_marks = 0;
+        cout << "Enter marks for " << num_subjects << " subjects:\n";
 
-        for(j = 1; j <= m; j++){
+        // Process marks for each subject
+        for (int j = 1; j <= num_subjects; ++j) {
             cin >> marks;
-            total = total + marks;
+            total_marks += marks;
         }
 
-        cout << "\nTotal Marks: " << total;
+        // Output the total marks for the current student
+        cout << "Total Marks for Roll Number " << roll_no << ": " << total_marks << endl;
     }
-
+    
     return 0;
 }
